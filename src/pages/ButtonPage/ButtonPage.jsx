f8a809ce6a979705ca7713a295ee48a6b9b22eb9
Lucas-ez <lucasezequiel237@gmail.com>
2023-03-24 15:55:44 -0300

    add button-4

--- src/pages/ButtonPage/ButtonPage.jsx
+++ src/pages/ButtonPage/ButtonPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import './ButtonPage.scss'
 
 const ButtonPage = () => {
   return (
-    <div className='page'>
-      ButtonPage
+    <div className='page button-page'>
+      <div className='container'>
+        <button className='type-1'>Button</button>
+        <button className='type-2'>Button</button>
+        <button className='type-4'>Button</button>
+        <button className='type-3'>Button</button>
+      </div>
     </div>
   )
 }
